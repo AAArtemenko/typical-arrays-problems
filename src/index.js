66ee460a5898f341c963d5e75b2c2f0e8363b66a
AAARTEMENKO <ar4ys@yandex.ru>
2020-03-21 18:02:55 +0300

    feat: complete task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
 exports.min = function min (array) {
+  let min = Number.MAX_SAFE_INTEGER;
+  if (array !== undefined && array.length > 0) {
+    array.forEach(element => {
+      if (element < min) {
+        min = element;
+      }
+    });   
+    return min; 
+  } 
   return 0;
 }
 
 exports.max = function max (array) {
+  let max = Number.MIN_SAFE_INTEGER;
+  if (array !== undefined && array.length > 0) {
+    array.forEach(element => {
+      if (element > max) {
+        max = element;
+      }
+    });   
+    return max; 
+  } 
   return 0;
 }
 
 exports.avg = function avg (array) {
+  let sum = 0;
+  if (array !== undefined && array.length > 0) {
+    array.forEach(element => {
+      sum += element;
+    });   
+    return +((sum/array.length).toFixed(3)); 
+  } 
   return 0;
 }
